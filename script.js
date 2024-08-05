91fe6fc20419edef2ac11798fd44abca841870e5
pancake <q1609357977@gmail.com>
2024-08-05 11:13:22 +0800

    Add "Shake" button for refresh, add random color

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const grid = document.querySelector(".container");
 grid.style.cssText = "display: flex; flex-direction: column;"
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.style.cssText = "display: flex";
-  for (let i = 0; i < 16; i++) {
-    const column = document.createElement("div");
-    column.style.cssText = "width: 40px; height: 40px; border: 1px solid black;";
-    column.addEventListener("mouseenter", (event) => event.target.style.backgroundColor = "purple");
-    row.appendChild(column);
+// Add "Shake" button to refresh grid
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => makeGrid(getSize()))
+
+makeGrid(16);
+
+function makeGrid(size) {
+  // Remove existing grid
+  grid.innerHTML = "";
+
+  // Create new grid
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.style.cssText = "display: flex";
+    for (let i = 0; i < size; i++) {
+      const column = document.createElement("div");
+      edgeLength = 960 / size + "px";
+      column.style.cssText = `width: ${edgeLength}; height: ${edgeLength}; border: 1px solid black;`;
+      // Set up hover effect
+      column.addEventListener("mouseenter", (event) => event.target.style.backgroundColor = getRandomColor());
+      row.appendChild(column);
+    }
+    grid.appendChild(row);
   }
-  grid.appendChild(row);
+}
+
+function getSize() {
+  let size;
+  while (true) {
+    size = +prompt("What's the number of squares per side for the new grid?", "Please enter positive integer that doesn't exceed 100");
+    if (Number.isInteger(size) && size <= 100 && size > 0) {
+      return size;
+    }
+  }
+}
+
+function getRandomColor() {
+  const digit = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += digit[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
